7bc4e0ec024bbbea6623728eed3e32ff37ad9a8d
Ian <ian.lc0018@outlook.com.br>
2020-04-02 21:00:00 -0300

    Add files via upload

--- desafio_60.py
+++ desafio_60.py
@@ -3,3 +3,23 @@
 # Ex:
 #
 # 5! = 5x4x3x2x1 = 120
+
+# EXEMPLO 1:
+
+# from math import factorial
+# n = int(input('Digite um número: '))
+# f = factorial(n)
+# print('O Fatorial de {} é {}'.format(n, f))
+
+# EXEMPLO 2:
+
+n = int(input('Digite um número: '))
+c = n
+fat = 1
+print('{}! = '.format(n), end='')
+while c > 0:
+    print('{} '.format(c), end='')
+    print('x ' if c > 1 else '= ', end='')
+    fat *= c
+    c -= 1
+print('{}'.format(fat))
