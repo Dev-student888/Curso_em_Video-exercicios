0df163f88faca041a41388adc088a13e8148de6b
Ian <ian.lc0018@outlook.com.br>
2020-04-30 00:16:50 -0300

    Add files via upload

--- desafio_62.py
+++ desafio_62.py
@@ -1,2 +1,21 @@
 # Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
 # O programa encerra quando ele disser que quer mostrar 0 termos.
+
+t = int(input('Digite o 1° termo de uma PA: '))
+r = int(input('Digite sua razão: '))
+e = 10
+c = 0
+tot = 0
+print('PA: ', end='')
+while e != 0:
+    while c < e:
+        print('{} '.format(t), end='')
+        print('=> ' if c < e else '', end='')
+        t += r
+        c += 1
+        tot += 1
+    print('PAUSA\n')
+    c = 0
+    e = int(input('Gostaria de ver quantos termos a mais? '))
+print('FIM!\n')
+print('Ao total você visualizou {} termos!\n'.format(tot))
