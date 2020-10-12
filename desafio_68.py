c7fa8a0cd507f067fafb4f43548b56623bd3ec8b
Ian <ian.lc0018@outlook.com.br>
2020-10-12 18:04:55 -0300

    Add files via upload

--- desafio_68.py
+++ desafio_68.py
@@ -1,20 +1,20 @@
 # Faça um programa que jogue par ou ímpar com o computador.
-# O jogo só será interrompido quando o jogador PERDER, mostrando o final
+# O jogo só será interrompido quando o jogador PERDER, mostrando o total
 # de vitórias consecutivas que ele conquistou no final do jogo.
 
 from random import randint
 print('=-'*15)
 print('VAMOS JOGAR PAR OU ÍMPAR')
 print('=-'*15)
+c = 0
 while True:
     n = int(input('Digite um valor: '))
-    r = str(input('Par ou Ímpar? [P/I] '))
+    r = str(input('Par ou Ímpar? [P/I] ')).upper()
     comp = randint(0, 10)
     s = n + comp
     if r in 'PI':
         print('-'*30)
         print(f'Você jogou {n} e o computador {comp}.', end='')
-        c = 0
         pi = ''
         if s % 2 == 0 and r == 'P' or s % 2 == 1 and r == 'I':
             if r == 'P':
@@ -23,8 +23,9 @@ while True:
                 pi = 'ÍMPAR'
             print(f'Total de {s} DEU {pi}! ')
             print('-'*30)
-            c += 1
             print('Você VENCEU!')
+            c += 1
+            print('Vamos jogar novamente...')
         if s % 2 == 1 and r == 'P' or s % 2 == 0 and r == 'I':
             if r == 'P':
                 pi = 'ÍMPAR'
@@ -36,7 +37,7 @@ while True:
             break
 
     if r not in 'PI':
-        print('Dado incorreto! Por favor digite novamente sua resposta!')
+        print('Dado incorreto! Por favor digite novamente sua resposta!\n')
 
 print('=-'*15)
 print(f'GAME OVER! Você venceu {c} vezes.')
