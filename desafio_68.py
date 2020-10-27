4708976ea406dfcc232f4bf0faa0914fb4bc7c1f
Ian <ian.lc0018@outlook.com.br>
2020-10-27 20:41:14 -0300

    Add files via upload

--- desafio_68.py
+++ desafio_68.py
@@ -3,9 +3,10 @@
 # de vitórias consecutivas que ele conquistou no final do jogo.
 
 from random import randint
-print('=-'*15)
+
+print('=-' * 15)
 print('VAMOS JOGAR PAR OU ÍMPAR')
-print('=-'*15)
+print('=-' * 15)
 c = 0
 while True:
     n = int(input('Digite um valor: '))
@@ -16,7 +17,7 @@ while True:
     comp = randint(0, 10)
     s = n + comp
     if r in 'PI':
-        print('-'*30)
+        print('-' * 30)
         print(f'Você jogou {n} e o computador {comp}.', end='')
         pi = ''
         if s % 2 == 0 and r == 'P' or s % 2 == 1 and r == 'I':
@@ -25,7 +26,7 @@ while True:
             else:
                 pi = 'ÍMPAR'
             print(f'Total de {s} DEU {pi}! ')
-            print('-'*30)
+            print('-' * 30)
             print('Você VENCEU!')
             c += 1
             print('Vamos jogar novamente...\n')
@@ -35,12 +36,12 @@ while True:
             else:
                 pi = 'PAR'
             print(f' Total de {s} DEU {pi}! ')
-            print('-'*30)
+            print('-' * 30)
             print('Você PERDEU!')
             break
 
     else:
         print('Dado incorreto! Por favor digite novamente sua resposta!\n')
 
-print('=-'*15)
+print('=-' * 15)
 print(f'GAME OVER! Você venceu {c} vezes.')
