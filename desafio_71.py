360cc5a57f930e6bea0701b408662f7a5f15a3b5
Ian <ian.lc0018@outlook.com.br>
2021-01-09 00:05:56 -0300

    Add files via upload

--- desafio_71.py
+++ desafio_71.py
@@ -3,3 +3,38 @@
 # e o programa vai informar quantas cédulas de cada valor serão entregues.
 #
 # OBS.: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
+def title(x):
+    print('='*x)
+    print(' '*int(x/3), end='')
+    print('BANCO CEV')
+    print('='*x)
+
+
+def eq(y):
+    print('='*y)
+
+
+title(30)
+v = int(input('Qual valor você quer sacar? '))
+total = v
+c = 50
+tc = 0
+while True:
+    if total >= c:
+        total -= c
+        tc += 1
+    else:
+        if tc > 0:
+            print(f'Total de {tc} cédulas de R${c}')
+        if c == 50:
+            c = 20
+        elif c == 20:
+            c = 10
+        elif c == 10:
+            c = 1
+        tc = 0
+        if total == 0:
+            break
+eq(30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
