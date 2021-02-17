fbe610057aa97316362f29165f3046feb0fdb43a
Ian <ian.lc0018@outlook.com.br>
2021-02-16 22:23:19 -0300

    Add files via upload

--- desafio_76.py
+++ desafio_76.py
@@ -1,3 +1,21 @@
 # Crie um programa que tenha uma tupla única com nomes de produtos e seus
 # respectivos preços, na sequência. No final, mostre uma listagem de preços,
 # organizando os dados de forma tabular.
+
+produtos = ('Lápis', 1.75,
+            'Borracha', 2,
+            'Caderno', 15.90,
+            'Estojo', 25,
+            'Trasferidor', 4.20,
+            'Compasso', 9.99,
+            'Mochila', 120.32,
+            'Canetas', 22.30,
+            'Livro', 34.90)
+
+print('-'*40, f'\n{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
+for item in range(len(produtos)):
+    if item % 2 == 0:
+        print(f'{produtos[item]:.<30}', end='')
+    else:
+        print(f'R${produtos[item]:>7.2f}')
