927738d1dbce1e49580055ff9638ba6183fc6654
Ian <ian.lc0018@outlook.com.br>
2021-02-22 03:26:51 -0300

    Add files via upload

--- desafio_78.py
+++ desafio_78.py
@@ -8,17 +8,11 @@ for i in range(5):
     l.append(int(input(f'Digite um valor para a Posição {i}: ')))
 print('=-' * 30)
 print(f'Você digitou os valores {l}')
-pos_max = list()
-if l.count(max(l)) >= 1:
-    for p in range(l.count(max(l)) + 1):
-        pos_max.append(l.index(max(l)))
 print(f'O maior valor digitado foi {max(l)} nas posições ', end='')
-for v in pos_max:
-    print(f'{v}... ', end='')
-pos_min = list()
-if l.count(min(l)) >= 1:
-    for p in range(l.count(min(l)) + 1):
-        pos_min.append(l.index(min(l)))
+for pos, v in enumerate(l):
+    if v == max(l):
+        print(f'{pos}... ', end='')
 print(f'\nO menor valor digitado foi {min(l)} nas posições ', end='')
-for v in pos_min:
-    print(f'{v}... ', end='')
+for pos, v in enumerate(l):
+    if v == min(l):
+        print(f'{pos}... ', end='')
