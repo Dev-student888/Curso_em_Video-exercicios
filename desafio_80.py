baf053e0895ef1623fa9ba921e65c86ea52cbee8
Ian <ian.lc0018@outlook.com.br>
2021-02-23 15:05:41 -0300

    Add files via upload

--- desafio_80.py
+++ desafio_80.py
@@ -1,3 +1,20 @@
 # Crie um programa onde o usuário possa digitar cinco valores numéricos
 # e cadastre-os em uma lista, já na posição correta de inserção
 # (sem usar o sort()). No final, mostre a lista ordenada na tela.
+
+l = []
+mj = mn = 0
+
+for i in range(0, 5):
+    l.append(int(input('Digite um número: ')))
+    if i == 0:
+        mj = mn = l[i]
+    else:
+        if l[i] < mn:
+            mn = l[i]
+            l[i-1] = mn
+        if l[i] > mj:
+            mj = l[i]
+            l[i+1] = mj
+
+print(f'Lista: {l}')
