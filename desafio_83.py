93d7cf34c2a8ea018c7465596f7dbb02d2d09ee9
Ian <ian.lc0018@outlook.com.br>
2021-02-24 02:41:27 -0300

    Add files via upload

--- desafio_83.py
+++ desafio_83.py
@@ -1,3 +1,19 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use
 # parênteses. Seu aplicativo deverá analisar se a expressão está com os
 # parênteses abertos e fechados na ordem correta.
+
+expr = str(input('Digite uma expressão: '))
+pilha = list()
+for símb in expr:
+    if símb == '(':
+        pilha.append('(')
+    elif símb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está correta!')
+else:
+    print('Sua expressão está errada!')
