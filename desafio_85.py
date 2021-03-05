b325cca6487739def0c8a41b638fa74357230fbf
Ian <ian.lc0018@outlook.com.br>
2021-03-05 10:36:34 -0300

    Add files via upload

--- desafio_85.py
+++ desafio_85.py
@@ -2,18 +2,18 @@
 # e cadastre-os em uma lista única que mantenha os valores pares e ímpares.
 # No final, mostre os valores pares e ímpares em ordem crescente.
 
-l = list()
-for i in range(1, 8):
-    l.append(int(input(f'Digite o {i}º valor: ')))
+l = [[], []]
+valor = 0
 
+for i in range(1, 8):
+    valor = int(input(f'Digite o {i}º valor: '))
+    if valor % 2 == 0:
+        l[0].append(valor)
+    else:
+        l[1].append(valor)
+
+l[0].sort()
+l[1].sort()
 print('-='*30)
-l.sort()
-print('Os valores pares digitados foram: [ ', end='')
-for p in l:
-    if p % 2 == 0:
-        print(f'{p}', end=' ')
-print(']\nOs valores ímpares digitados foram: [ ', end='')
-for p in l:
-    if p % 2 == 1:
-        print(f'{p}', end=' ')
-print(']')
+print(f'Os valores pares digitados foram: {l[0]}')
+print(f'Os valores ímpares digitados foram: {l[1]}')
