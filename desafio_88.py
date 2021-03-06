dd9b8b4414f74657a6e0033ec31b1989ea9744cf
Ian <ian.lc0018@outlook.com.br>
2021-03-06 20:58:49 -0300

    Add files via upload

--- desafio_88.py
+++ desafio_88.py
@@ -2,3 +2,15 @@
 # palpites. O programa vai perguntar quantos jogos serão gerados
 # e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo
 # em uma lista composta.
+
+from random import randint
+l = list()
+
+j = int(input('Quantos jogos você quer gerar? '))
+for u in range(0, j):
+    l.append([])
+
+for i in range(1, j + 1):
+    print(f'Resultado do {i}º jogo: ')
+
+print(l)
