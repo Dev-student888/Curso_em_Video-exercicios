343f36e223ee618d5b77d5555a3e132d03d33ed5
Ian <ian.lc0018@outlook.com.br>
2021-03-18 02:21:49 -0300

    Add files via upload

--- desafio_93.py
+++ desafio_93.py
@@ -6,14 +6,29 @@
 # o campeonato.
 
 jogador = dict()
+gols = list()
 t = 0
-jogador['nome'] = str(input('Nome: ')).strip()
-jogador['partidas'] = int(input(f'Quantidade de partidas que {jogador["nome"]} jogou: '))
-for i in range(jogador['partidas']):
-    gp = int(input(f'Gols da {i + 1}ª partida: '))
+jogador['nome'] = str(input('Nome do Jogador: ')).strip()
+partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for i in range(partidas):
+    gp = int(input(f'Quantos gols na {i + 1}ª partida? '))
     t += gp
-jogador['total de gols'] = t
+    gols.append(gp)
+jogador['gols'] = gols[:]
+jogador['total'] = t
+# jogador['total'] = sum(gols)
 print('-=' * 30)
-print(' == RANKING DO JOGADOR ==')
+print(jogador)
+print('-=' * 30)
 for k, v in jogador.items():
-    print(f'  - {k} tem o valor {v}')
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 30)
+print(f'O jogador {jogador["nome"]} jogou {partidas} partidas.')
+for i in range(partidas):
+    for j in gols:
+        print(f'\t=> Na {i + 1}ª partida, fez {j} gols.')
+        i += 1
+    break
+# for i, v in enumerate(jogador['gols']):
+#     print(f'\t=>Na {i + 1}ª partida, fez {v} gols.')
+print(f'Foi um total de {t} gols.')
