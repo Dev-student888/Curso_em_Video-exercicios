0e86513e6f9b799e7d9a59a201db703b2e92163b
Ian <ian.lc0018@outlook.com.br>
2021-03-21 11:41:26 -0300

    Add files via upload

--- desafio_93.py
+++ desafio_93.py
@@ -1,9 +1,9 @@
 # Crie um programa que gerencie o aproveitamento de um
 # jogador de futebol. O programa vai ler o nome do jogador
 # e quantas partidas ele jogou. Depois vai ler a quantidade
-# de gols feitos em cada partida. No final, tudo isso será guardado
-# em um dicionário, incluindo o total de gols feitos durante
-# o campeonato.
+# de gols feitos em cada partida. No final, tudo isso será
+# guardado em um dicionário, incluindo o total de gols
+# feitos durante o campeonato.
 
 jogador = dict()
 gols = list()
