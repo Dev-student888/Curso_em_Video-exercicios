c380b7f4da154a2638ed16f6e31b55fe5a9a00c1
Ian <ian.lc0018@outlook.com.br>
2021-03-26 09:54:35 -0300

    Add files via upload

--- desafio_100.py
+++ desafio_100.py
@@ -3,3 +3,30 @@
 # vai sortear 5 números e vai colocá-los dentro da lista e a
 # segunda função vai mostrar a SOMA entre todos os valores pares
 # sorteados pela função anterior.
+from random import randint
+from time import sleep
+
+números = list()
+
+
+def sorteia():
+    print('Sorteando 5 valores da lista: ', end='')
+    for i in range(0, 5):
+        números.append(randint(0, 10))
+
+    for i in números:
+        print(i, end=' ')
+        sleep(0.5)
+    print('PRONTO!')
+
+
+def somapar():
+    s = 0
+    for i in números:
+        if i % 2 == 0:
+            s += i
+    print(f'Somando os valores pares de {números}, temos {s}')
+
+
+sorteia()
+somapar()
