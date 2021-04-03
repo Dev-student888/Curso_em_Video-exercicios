ffe26ab50ede6d6a3af014cac61893c6fd9a6299
Ian <ian.lc0018@outlook.com.br>
2021-04-03 00:44:24 -0300

    Add files via upload

--- desafio_102.py
+++ desafio_102.py
@@ -3,3 +3,27 @@
 # calcular e o outro chamado show(), que será um valor lógico
 # [opcional] indicando se será mostrado ou não na tela
 # o processo de cálculo do fatorial.
+
+def fatorial(n, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param n: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    """
+    print('-' * 30)
+    if show is False:
+        f = 1
+        for i in range(n, 0, -1):
+            f *= i
+        return f
+    else:
+        f = 1
+        for i in range(n, 0, -1):
+            f *= i
+            print(str(i) + ' x ' if i >= 2 else str(i) + ' = ', end='')
+        return f
+
+
+print(fatorial(5))
+print(fatorial(5, True))
