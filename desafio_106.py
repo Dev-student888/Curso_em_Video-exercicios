403e1bb82a1088bdc280d28289bf47eac008eea8
Ian <ian.lc0018@outlook.com.br>
2021-04-05 00:29:50 -0300

    Add files via upload

--- desafio_106.py
+++ desafio_106.py
@@ -6,20 +6,20 @@
 
 def ajuda():
     while True:
-        print("\033[0;30m" + "~~" + "~" * 23 + "~~" + "\033[m")
-        print("\033[0;30m  SISTEMA DE AJUDA PyHELP  \033[m")
-        print("\033[0;30m" + "~~" + "~" * 23 + "~~" + "\033[m")
+        print("\033[0;30;44m" + "~~" + "~" * 23 + "~~" + "\033[m")
+        print("\033[0;30;44m  SISTEMA DE AJUDA PyHELP  \033[m")
+        print("\033[0;30;44m" + "~~" + "~" * 23 + "~~" + "\033[m")
         valor = str(input("Função ou Biblioteca >"))
+        if valor in 'FIMfim':
+            print("\033[0;31;40m" + "~" * 4 + "~" * 9 + "\033[m")
+            print("\033[0;31;40m  ATÉ LOGO!  \033[m")
+            print("\033[0;31;40m" + "~" * 4 + "~" * 9 + "\033[m")
+            break
         if valor.isidentifier():
             print("\033[0;30;42m" + "~" * 36 + "~" * len(valor) + "\033[m")
             print(f"\033[0;30;42m  Acessando o manual do comando \'{valor}\'  \033[m")
             print("\033[0;30;42m" + "~" * 36 + "~" * len(valor) + "\033[m")
             help(valor)
-        if valor in 'FIMfim':
-            print("\033[0;31;40m" + "~~" + "~" * 9 + "~~" + "\033[m")
-            print("\033[0;31;40m  ATÉ LOGO!  \033[m")
-            print("\033[0;31;40m" + "~~" + "~" * 9 + "~~" + "\033[m")
-            break
 
 
 ajuda()
