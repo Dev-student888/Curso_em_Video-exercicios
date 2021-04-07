0a4091208b100d2069eaf04ee17ff0327eb41bdb
Ian <ian.lc0018@outlook.com.br>
2021-04-06 21:48:21 -0300

    Add files via upload

--- desafio_107.py
+++ desafio_107.py
@@ -1,9 +1,9 @@
-# Crie um módulo chamado moeda.py que tenha as funções
+# Crie um módulo chamado moeda_107.py que tenha as funções
 # incorporadas aumentar(), diminuir(), dobro() e metade().
 # Faça também um programa que importe esse módulo e use algumas dessas
 # funções.
 
-from moeda import aumentar, diminuir, dobro, metade
+from moeda_107 import aumentar, diminuir, dobro, metade
 
 p = float(input('Digite o preço: R$'))
 print(f'A metade de {p} é R${metade(p)}')
