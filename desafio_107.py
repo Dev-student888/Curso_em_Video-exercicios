60b60371235236701b9849a26d66dc8731bb74f0
Ian <ian.lc0018@outlook.com.br>
2021-04-06 21:41:27 -0300

    Add files via upload

--- desafio_107.py
+++ desafio_107.py
@@ -6,7 +6,7 @@
 from moeda import aumentar, diminuir, dobro, metade
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {int(p)} é R${metade(p):.2f}')
-print(f'O dobro de {int(p)} é R${dobro(p):.2f}')
-print(f'Aumentando 10%, temos R${aumentar(p, 10):.2f}')
-print(f'Reduzindo 13%, temos R${diminuir(p, 13):.2f}')
+print(f'A metade de {p} é R${metade(p)}')
+print(f'O dobro de {p} é R${dobro(p)}')
+print(f'Aumentando 10%, temos R${aumentar(p, 10)}')
+print(f'Reduzindo 13%, temos R${diminuir(p, 13)}')
