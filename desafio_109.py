52f8eb9b2e70c018abeb9692a7e7d228d98e23ec
Ian <ian.lc0018@outlook.com.br>
2021-04-07 02:01:31 -0300

    Add files via upload

--- desafio_109.py
+++ desafio_109.py
@@ -2,3 +2,11 @@
 # elas aceitem um parâmetro a mais, informando se o valor
 # retornado por elas vai ser ou não formatado pela função moeda(),
 # desenvolvida no desafio 108.
+
+from moeda_109 import metade, dobro, aumentar, diminuir, moeda
+
+p = float(input('Digite o preço: '))
+print(f'A metade de {moeda(p)} é {metade(p, True)}')
+print(f'O dobro de {moeda(p)} é {dobro(p, True)}')
+print(f'Aumentando 10%, temos {aumentar(p, 10, True)}')
+print(f'Reduzindo 13%, temos {diminuir(p, 13, True)}')
