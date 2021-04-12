fa0893322a56a9ca93b77e4cd005ad39a14e6d14
Ian <ian.lc0018@outlook.com.br>
2021-04-12 04:22:00 -0300

    Add files via upload

--- desafio_114.py
+++ desafio_114.py
@@ -1,2 +1,16 @@
 # Crie um programa em Python que teste se o site Pudim está
 # acessível pelo computador usado.
+
+
+def internet():
+    import urllib.error
+    from urllib import request
+    try:
+        request.urlopen('http://www.pudim.com.br')
+    except urllib.error.URLError:
+        print('\033[0;31mO site Pudim não está acessível no momento!\033[m')
+    else:
+        print('\033[0;32mConsegui acessar o site Pudim com sucesso!\033[m')
+
+
+internet()
--- desafio_115.py
+++ desafio_115.py
@@ -2,3 +2,7 @@
 # pessoas pelo seu nome e idade em um arquivo de texto simples.
 # O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar
 # todas as pessoas cadastradas.
+
+from desafio115_funcoes import opções
+
+opções()
