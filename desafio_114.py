b5a9e64d2df65b887c0e8d704a9126f4192c4753
Ian <ian.lc0018@outlook.com.br>
2021-04-21 21:50:13 -0300

    Add files via upload

--- desafio_114.py
+++ desafio_114.py
@@ -3,11 +3,11 @@
 
 
 def internet():
-    import urllib.error
+    from urllib.error import URLError
     from urllib import request
     try:
         request.urlopen('http://www.pudim.com.br')
-    except urllib.error.URLError:
+    except URLError:
         print('\033[0;31mO site Pudim não está acessível no momento!\033[m')
     else:
         print('\033[0;32mConsegui acessar o site Pudim com sucesso!\033[m')
